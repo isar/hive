2cb551ed20256e86d3b590728a144da8d33b0aa5
Kiesman <justin.vietz@gmx.de>
2019-07-27 11:55:26 +0200

    Corrected typo (#5)
    stronly -> strongly

--- hive/lib/hive.dart
+++ hive/lib/hive.dart
@@ -1,4 +1,4 @@
-/// Hive is a lightweight and blazing fast key-value store written in pure Dart. It is stronly encrypted using AES-256.
+/// Hive is a lightweight and blazing fast key-value store written in pure Dart. It is strongly encrypted using AES-256.
 library hive;
 
 import 'dart:async';
