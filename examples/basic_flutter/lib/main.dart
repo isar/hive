d16d57dd88571a6a55bb27af93de1f9b93531854
Simon Leier <simonleier@gmail.com>
2019-08-19 22:36:31 +0200

    hive_flutter initial commit

--- examples/basic_flutter/lib/main.dart
+++ examples/basic_flutter/lib/main.dart
@@ -1,72 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() async {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Future<Box> _openBox() async {
-    final directory = await getApplicationDocumentsDirectory();
-    Hive.init(directory.path);
-    return await Hive.openBox('myBox');
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Hive Demo',
       home: Scaffold(
-        body: FutureBuilder<Box>(
-          future: _openBox(),
-          builder: (BuildContext context, AsyncSnapshot<Box> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return IceCreamPage();
-            } else {
-              return Center(
-                child: Text('Please wait'),
-              );
-            }
+        body: HiveBuilder.openBox(
+          openBox: () async {
+            var dir = await getApplicationDocumentsDirectory();
+            Hive.init(dir.path);
+            var box = await Hive.openBox('testBox');
+            return box;
           },
+          builder: _buildCounter,
+          child: Center(
+            child: Text('Please wait...'),
+          ),
         ),
       ),
     );
   }
-}
 
-class IceCreamPage extends StatefulWidget {
-  @override
-  _IceCreamPageState createState() => _IceCreamPageState();
-}
-
-class _IceCreamPageState extends State<IceCreamPage> {
-  var box = Hive.box('myBox');
-
-  @override
-  void initState() {
-    super.initState();
-
-    box.watch().listen((e) {
-      setState(() {});
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var userLikesIceCream = box.get('userLikesIceCream', defaultValue: false);
+  Widget _buildCounter(context, box) {
+    var number = box.get('number', defaultValue: 0);
     return Center(
-      child: Row(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text(userLikesIceCream
-              ? 'You like ice cream'
-              : 'You do not like ice cream. What is wrong with you?'),
-          Checkbox(
-            value: userLikesIceCream,
-            onChanged: (selection) async {
-              await box.put('userLikesIceCream', selection);
-            },
+          Text(
+            'You have pushed the button $number times.',
+            style: TextStyle(fontSize: 18),
           ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  box.put('number', number + 1);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Increase',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+              SizedBox(width: 20),
+              RaisedButton(
+                onPressed: () {
+                  box.put('number', 0);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Reset',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
