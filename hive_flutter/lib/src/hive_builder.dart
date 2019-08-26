021a298fa47e4f1a610e3f97fd42ae0884460790
TheDOme6 <dominik2003schulze@gmail.com>
2019-08-26 15:20:13 +0200

    add docs

--- hive_flutter/lib/src/hive_builder.dart
+++ hive_flutter/lib/src/hive_builder.dart
@@ -1,25 +1,41 @@
-//TODO: Add documentation
-
 part of hive_flutter;
 
-typedef AsyncBoxWidgetBuilder = Widget Function(BuildContext context, Box box);
+/// Signature for a function that builds a widget with an error [Object].
 typedef ErrorBoxWidgetBuilder = Widget Function(
     BuildContext context, Object error);
+
+/// Signature for a function that builds a widget when the [Box] is
+/// currently loading.
 typedef LoadingBoxWidgetBuilder = Widget Function(BuildContext context);
 
+/// A general-purpose widget which rebuilds itself when the [Box] in the
+/// [openBox] function is loaded. While the box is not loaded a placeholder
+/// [placeholderBuilder] is specified to inform the user. In the case of the
+/// box failing to open an error widget [errorBuilder] is displayed.
 class HiveBuilder extends StatelessWidget {
-  final Future<Box> Function() openBox;
-  final AsyncBoxWidgetBuilder builder;
-  final LoadingBoxWidgetBuilder placeholderBuilder;
-  final ErrorBoxWidgetBuilder errorBuilder;
-
-  const HiveBuilder.openBox(
+  HiveBuilder.openBox(
       {Key key,
       @required this.openBox,
       @required this.builder,
       @required this.placeholderBuilder,
       @required this.errorBuilder})
-      : super(key: key);
+      : assert(openBox != null),
+        assert(builder != null),
+        assert(errorBuilder != null),
+        assert(placeholderBuilder != null),
+        super(key: key);
+
+  /// The function where the box is opened
+  final Future<Box> Function() openBox;
+
+  /// Called when the box finished loading
+  final BoxWidgetBuilder builder;
+
+  /// Called while the box is loading
+  final LoadingBoxWidgetBuilder placeholderBuilder;
+
+  /// Called when the box finished loading with an error
+  final ErrorBoxWidgetBuilder errorBuilder;
 
   @override
   Widget build(BuildContext context) {
