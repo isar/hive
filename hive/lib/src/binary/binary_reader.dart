bd53c4023d385a09e0f7473bda044500943100f0
Simon Leier <simonleier@gmail.com>
2019-09-09 19:45:11 +0200

    Improve docs

--- hive/lib/src/binary/binary_reader.dart
+++ hive/lib/src/binary/binary_reader.dart
@@ -18,9 +18,11 @@ abstract class BinaryReader {
   /// Read a single byte.
   int readByte();
 
-  /// Get a [Uint8List] view from
+  /// Get a [Uint8List] view which contains the next [bytes] bytes.
   Uint8List viewBytes(int bytes);
 
+  /// Get a [Uint8List] view which contains the next [bytes] bytes. This does
+  /// not advance the internal read position.
   Uint8List peekBytes(int bytes);
 
   /// Read two bytes as 16-bit unsigned integer.
@@ -43,7 +45,7 @@ abstract class BinaryReader {
 
   /// Read [byteCount] bytes and decode an UTF-8 String.
   ///
-  /// If [byteCount] is not provided, it is also read from the disk.
+  /// If [byteCount] is not provided, it is read first.
   String readString([
     int byteCount,
     Converter<List<int>, String> decoder = utf8Decoder,
@@ -51,32 +53,32 @@ abstract class BinaryReader {
 
   /// Read an ASCII-String with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   String readAsciiString([int length]);
 
   /// Read a list of bytes with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   Uint8List readByteList([int length]);
 
   /// Read a list of integers with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   List<int> readIntList([int length]);
 
   /// Read a list of doubles with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   List<double> readDoubleList([int length]);
 
   /// Read a list of booleans with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   List<bool> readBoolList([int length]);
 
   /// Read a list of Strings with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   List<String> readStringList([
     int length,
     Converter<List<int>, String> decoder = utf8Decoder,
@@ -84,16 +86,16 @@ abstract class BinaryReader {
 
   /// Read a list with [length].
   ///
-  /// If [length] is not provided, it is also read from the disk.
+  /// If [length] is not provided, it is read first.
   List readList([int length]);
 
   /// Read a map with [length] entries.
   ///
-  /// If [length] is not provided, it is also read.
+  /// If [length] is not provided, it is read first.
   Map readMap([int length]);
 
   /// Read and decode any value.
   ///
-  /// If [typeId] is not provided, it is also read.
+  /// If [typeId] is not provided, it is read first.
   dynamic read([int typeId]);
 }
