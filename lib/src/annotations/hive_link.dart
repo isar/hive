e8f08c1f86904c2cc0ebe1d86fe68fcd4be333f0
Simon Leier <simonleier@gmail.com>
2019-12-11 23:09:45 +0100

    Delete hive_link.dart

--- lib/src/annotations/hive_link.dart
+++
@@ -1,9 +0,0 @@
-part of hive;
-
-/// Annotate fields with [HiveLink] to link them to other [HiveObject]s.
-class HiveLink {
-  /// The box that contains the other [HiveObject].
-  final String boxName;
-
-  const HiveLink(this.boxName);
-}
