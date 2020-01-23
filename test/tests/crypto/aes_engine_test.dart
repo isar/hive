1d57c768cbc5d57728e8121f8d59d71b79139ce7
Simon Leier <simonleier@gmail.com>
2020-01-23 23:46:42 +0100

    Fix AESEngine test

--- test/tests/crypto/aes_engine_test.dart
+++ test/tests/crypto/aes_engine_test.dart
@@ -1,6 +1,8 @@
 import 'dart:typed_data';
 
 import 'package:hive/src/crypto/aes_engine.dart';
+import 'package:pointycastle/api.dart';
+import 'package:pointycastle/block/aes_fast.dart';
 import 'package:test/test.dart';
 
 import 'message.dart';
@@ -14,18 +16,34 @@ void main() {
 
     test('.encryptBlock()', () {
       var out = Uint8List(message.length);
+
+      var pcEngine = AESFastEngine();
+      var outPc = Uint8List(message.length);
+
       for (var i = 0; i < message.length; i += aesBlockSize) {
-        //encryptBlock(encryptionKey, message, i, out, i);
+        encryptBlock(encryptionKey, message, i, out, i);
+        pcEngine.init(true, KeyParameter(key));
+        pcEngine.processBlock(message, i, outPc, i);
       }
-      //expect(out, encrypted);
+      expect(out, outPc);
     });
 
     test('.decryptBlock()', () {
-      var out = Uint8List(256);
+      var out = Uint8List(message.length);
+
+      var pcEngine = AESFastEngine();
+      var encrypted = Uint8List(message.length);
+
       for (var i = 0; i < message.length; i += aesBlockSize) {
-        //decryptBlock(decryptionKey, encrypted, i, out, i);
+        decryptBlock(encryptionKey, message, i, out, i);
+        pcEngine.init(true, KeyParameter(key));
+        pcEngine.processBlock(message, i, encrypted, i);
       }
-      //expect(out, message);
+
+      for (var i = 0; i < encrypted.length; i += aesBlockSize) {
+        decryptBlock(decryptionKey, encrypted, i, out, i);
+      }
+      expect(out, message);
     });
   });
 }
