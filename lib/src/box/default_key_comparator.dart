804b9b8f628b9c37c681f04bd0db03c80822e62e
Simon Leier <simonleier@gmail.com>
2020-02-08 01:14:37 +0100

    Improve defaultKeyComparator performance

--- lib/src/box/default_key_comparator.dart
+++ lib/src/box/default_key_comparator.dart
@@ -1,7 +1,13 @@
 int defaultKeyComparator(dynamic k1, dynamic k2) {
-  if (k1.runtimeType == k2.runtimeType) {
-    return (k1 as Comparable).compareTo(k2);
+  if (k1 is int) {
+    if (k2 is int) {
+      return k1.compareTo(k2);
+    } else {
+      return -1;
+    }
+  } else if (k2 is String) {
+    return (k1 as String).compareTo(k2);
   } else {
-    return k1 is int ? -1 : 1;
+    return 1;
   }
 }
