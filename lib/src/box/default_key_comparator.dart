70bf09729cc821a52151cd0a961e206da0695f55
Simon Leier <simonleier@gmail.com>
2020-02-12 18:57:55 +0100

    Improve defaultKeyComparator

--- lib/src/box/default_key_comparator.dart
+++ lib/src/box/default_key_comparator.dart
@@ -1,7 +1,13 @@
 int defaultKeyComparator(dynamic k1, dynamic k2) {
   if (k1 is int) {
     if (k2 is int) {
-      return k1.compareTo(k2);
+      if (k1 > k2) {
+        return 1;
+      } else if (k1 < k2) {
+        return -1;
+      } else {
+        return 0;
+      }
     } else {
       return -1;
     }
