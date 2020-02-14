abbe51ce1dbce9e6c1e0dca18fb8d3280de9632f
Simon Leier <simonleier@gmail.com>
2020-02-14 23:53:48 +0100

    Remove duplicate await

--- hive/test/integration/put_many_simultaneously_test.dart
+++ hive/test/integration/put_many_simultaneously_test.dart
@@ -21,7 +21,7 @@ Future _performTest(bool lazy) async {
 
   box = await box.reopen();
   for (var i = 0; i < amount; i++) {
-    expect(await await box.get('key$i'), 'value$i');
+    expect(await box.get('key$i'), 'value$i');
   }
   await box.close();
 }
