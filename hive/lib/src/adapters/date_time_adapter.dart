160589fec97d7babf65a9c414285c48be210814f
Misir Jafarov <misir.ceferov@gmail.com>
2020-09-27 14:29:05 +0400

    Add new DateTimeWithTimezoneAdapter (fixes #419)

--- hive/lib/src/adapters/date_time_adapter.dart
+++ hive/lib/src/adapters/date_time_adapter.dart
@@ -16,3 +16,22 @@ class DateTimeAdapter extends TypeAdapter<DateTime> {
     writer.writeInt(obj.millisecondsSinceEpoch);
   }
 }
+
+/// Alternative adapter for DateTime with TimeZone info
+class DateTimeWithTimezoneAdapter extends TypeAdapter<DateTime> {
+  @override
+  final typeId = 16;
+
+  @override
+  DateTime read(BinaryReader reader) {
+    var micros = reader.readInt();
+    var isUtc = reader.availableBytes > 0 ? reader.readBool() : false;
+    return DateTime.fromMillisecondsSinceEpoch(micros, isUtc: isUtc);
+  }
+
+  @override
+  void write(BinaryWriter writer, DateTime obj) {
+    writer.writeInt(obj.millisecondsSinceEpoch);
+    writer.writeBool(obj.isUtc);
+  }
+}
--- hive/test/tests/adapters/date_time_adapter_test.dart
+++ hive/test/tests/adapters/date_time_adapter_test.dart
@@ -24,4 +24,72 @@ void main() {
       verify(binaryWriter.writeInt(now.millisecondsSinceEpoch));
     });
   });
+
+  group('DateTimeWithTimezoneAdapter', () {
+    group('.read()', () {
+      test('local', () {
+        var now = DateTime.now();
+        var binaryReader = BinaryReaderMock();
+        when(binaryReader.readInt()).thenReturn(now.millisecondsSinceEpoch);
+        when(binaryReader.readBool()).thenReturn(false);
+        when(binaryReader.availableBytes).thenReturn(1);
+
+        var date = DateTimeWithTimezoneAdapter().read(binaryReader);
+        verifyInOrder([binaryReader.readInt(), binaryReader.readBool()]);
+        expect(date, now.subtract(Duration(microseconds: now.microsecond)));
+      });
+
+      group('UTC', () {
+        test('with timezone info', () {
+          var now = DateTime.now().toUtc();
+          var binaryReader = BinaryReaderMock();
+          when(binaryReader.readInt()).thenReturn(now.millisecondsSinceEpoch);
+          when(binaryReader.readBool()).thenReturn(true);
+          when(binaryReader.availableBytes).thenReturn(1);
+
+          var date = DateTimeWithTimezoneAdapter().read(binaryReader);
+          verifyInOrder([binaryReader.readInt(), binaryReader.readBool()]);
+          expect(date, now.subtract(Duration(microseconds: now.microsecond)));
+          expect(date.isUtc, true);
+        });
+
+        test('without timezone info', () {
+          var now = DateTime.now().toUtc();
+          var binaryReader = BinaryReaderMock();
+          when(binaryReader.readInt()).thenReturn(now.millisecondsSinceEpoch);
+          when(binaryReader.availableBytes).thenReturn(0);
+
+          var date = DateTimeWithTimezoneAdapter().read(binaryReader);
+          verify(binaryReader.readInt());
+          verifyNever(binaryReader.readBool());
+          expect(date.millisecondsSinceEpoch, now.millisecondsSinceEpoch);
+          expect(date.isUtc, false);
+        });
+      });
+    });
+
+    group('.write()', () {
+      test('local', () {
+        var now = DateTime.now();
+        var binaryWriter = BinaryWriterMock();
+
+        DateTimeWithTimezoneAdapter().write(binaryWriter, now);
+        verifyInOrder([
+          binaryWriter.writeInt(now.millisecondsSinceEpoch),
+          binaryWriter.writeBool(false),
+        ]);
+      });
+
+      test('UTC', () {
+        var now = DateTime.now().toUtc();
+        var binaryWriter = BinaryWriterMock();
+
+        DateTimeWithTimezoneAdapter().write(binaryWriter, now);
+        verifyInOrder([
+          binaryWriter.writeInt(now.millisecondsSinceEpoch),
+          binaryWriter.writeBool(true),
+        ]);
+      });
+    });
+  });
 }
