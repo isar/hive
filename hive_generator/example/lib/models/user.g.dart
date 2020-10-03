cead2b395140cf0395ffd9f120da7b82469b7d12
Misir Jafarov <misir.ceferov@gmail.com>
2020-10-03 20:48:03 +0400

    Fixes #455

---
+++ hive_generator/example/lib/models/user.g.dart
@@ -0,0 +1,75 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UserRoleAdapter extends TypeAdapter<UserRole> {
+  @override
+  final int typeId = 2;
+
+  @override
+  UserRole read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      default:
+        return null;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, UserRole obj) {
+    switch (obj) {
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserRoleAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class UserAdapter extends TypeAdapter<User> {
+  @override
+  final int typeId = 1;
+
+  @override
+  User read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return User()
+      ..firstName = fields[0] as String
+      ..lastName = fields[1] as String
+      ..birthday = fields[2] as DateTime;
+  }
+
+  @override
+  void write(BinaryWriter writer, User obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.firstName)
+      ..writeByte(1)
+      ..write(obj.lastName)
+      ..writeByte(2)
+      ..write(obj.birthday);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
