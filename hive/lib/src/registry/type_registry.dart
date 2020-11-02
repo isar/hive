3d7d9cbe56d74f4df745ce6c58ae6cbe2d753ef5
Misir Jafarov <misir.ceferov@gmail.com>
2020-11-02 14:37:53 +0400

    Adds support for overriding adapters

--- hive/lib/src/registry/type_registry.dart
+++ hive/lib/src/registry/type_registry.dart
@@ -5,7 +5,17 @@ part of hive;
 /// TypeIds have to be unique and must not change.
 abstract class TypeRegistry {
   /// Register a [TypeAdapter] to announce it to Hive.
-  void registerAdapter<T>(TypeAdapter<T> adapter);
+  ///
+  /// Tries to register [adapter] to registry. If another adapter with same
+  /// typeId had been already registered an exception will thrown or the adapter
+  /// will be overridden if [override] set to `true`. Please note that internal
+  /// adapters are registered and maintained by hive itself. Use [internal]
+  /// parameter only if you want to override exists adapter implementation.
+  void registerAdapter<T>(
+    TypeAdapter<T> adapter, {
+    bool internal = false,
+    bool override = false,
+  });
 
   /// Returns true if a [TypeAdapter] is registered
   bool isAdapterRegistered(int typeId);
