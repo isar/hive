daf1b686cb7c02450ef0c48c8208b5730c5b4b0d
Misir Jafarov <misir.ceferov@gmail.com>
2021-06-20 13:32:55 +0400

    Replace flutter_test with test and fix test CI

--- hive_flutter/test/adapters/color_adapter_test.dart
+++ hive_flutter/test/adapters/color_adapter_test.dart
@@ -1,6 +1,6 @@
 import 'dart:ui' show Color;
 
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mockito/mockito.dart';
