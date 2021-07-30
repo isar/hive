88ef50b1634a6d67ab678f4324f412ca9edb677a
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2021-07-30 21:24:47 +0100

    Instead of copying all .lock files, copy .lock files with accompanying .hive files

--- hive_flutter/lib/src/hive_extensions.dart
+++ hive_flutter/lib/src/hive_extensions.dart
@@ -27,25 +27,28 @@ extension HiveX on HiveInterface {
     // If the old location exists, we start looking for old DB files to copy.
     if (await oldLocationDir.exists()) {
       // Make an array to store files to copy.
-      final filesToCopy = <FileSystemEntity>[];
+      final filesToCopy = <File>[];
 
       await oldLocationDir.list().forEach((event) {
         final extension = path.extension(event.path);
 
-        // If the item is a file and has an extension of .hive/.lock, add it to
-        // the copy array.
-        if (extension is File &&
-            (extension == '.hive' || extension == '.lock')) {
-          filesToCopy.add(event);
+        // If the item is a file and has an extension of .hive, add it and the
+        // matching .lock file to the copy array.
+        if (event is File && extension == '.hive') {
+          filesToCopy.add(File(event.path));
+
+          // We do this instead of just copying all .lock files since .lock can
+          // commonly be used for non-Hive purposes.
+          filesToCopy.add(File(event.path.replaceAll('.hive', '.lock')));
         }
       });
 
       // Make copy/delete futures for every file and wait for them
       // asynchronously. Files are copied to the new appDir.
       await Future.wait(filesToCopy.map(
-        (e) => File(e.path)
+        (e) => e
             .copy(path_helper.join(appDir, path_helper.basename(e.path)))
-            .then((value) => File(e.path).delete()),
+            .then((_) => e.delete()),
       ));
     }
 
