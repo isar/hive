isar/hive

commit 4dbcd767e63d207c9144055ce45381c8f241ffed

feat: upstream FluffyBox patches (#956)
- add `BoxCollection`
- prepare support for other web backends

Fixes: #939

Signed-off-by: TheOneWithTheBraid <the-one@with-the-braid.cf>

hive/lib/src/hive_impl.dart
@@ -22,7 +22,7 @@ import 'backend/storage_backend.dart';
 class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   final _boxes = HashMap<String, BoxBaseImpl>();
   final _openingBoxes = HashMap<String, Future>();
-  final BackendManager _manager;
+  BackendManagerInterface? _manager;
   final Random _secureRandom = Random.secure();
 
   /// Not part of public API
@@ -30,9 +30,7 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   String? homePath;
 
   /// Not part of public API
-  HiveImpl() : _manager = BackendManager() {
-    _registerDefaultAdapters();
-  }
+  HiveImpl();
 
   /// Not part of public API
   @visibleForTesting
@@ -42,7 +40,7 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
 
   /// Not part of public API
   @visibleForTesting
-  HiveImpl.test() : _manager = BackendManager() {
+  HiveImpl.test() : _manager = BackendManager.select() {
     registerAdapter(DateTimeAdapter<DateTime>(), internal: true);
     registerAdapter(BigIntAdapter(), internal: true);
   }
@@ -54,8 +52,14 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   }
 
   @override
-  void init(String path) {
+  void init(
+    String? path, {
+    HiveStorageBackendPreference backendPreference =
+        HiveStorageBackendPreference.native,
+  }) {
     homePath = path;
+    _manager ??= BackendManager.select(backendPreference);
+    _registerDefaultAdapters();
   }
 
   Future<BoxBase<E>> _openBox<E>(
@@ -67,6 +71,7 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
     bool recovery,
     String? path,
     Uint8List? bytes,
+    String? collection,
   ) async {
     assert(path == null || bytes == null);
     assert(name.length <= 255 && name.isAscii,
@@ -97,8 +102,8 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
         if (bytes != null) {
           backend = StorageBackendMemory(bytes, cipher);
         } else {
-          backend =
-              await _manager.open(name, path ?? homePath, recovery, cipher);
+          backend = await _manager!
+              .open(name, path ?? homePath, recovery, cipher, collection);
         }
 
         if (lazy) {
@@ -132,13 +137,14 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
     bool crashRecovery = true,
     String? path,
     Uint8List? bytes,
+    String? collection,
     @Deprecated('Use encryptionCipher instead') List<int>? encryptionKey,
   }) async {
     if (encryptionKey != null) {
       encryptionCipher = HiveAesCipher(encryptionKey);
     }
     return await _openBox<E>(name, false, encryptionCipher, keyComparator,
-        compactionStrategy, crashRecovery, path, bytes) as Box<E>;
+        compactionStrategy, crashRecovery, path, bytes, collection) as Box<E>;
   }
 
   @override
@@ -149,13 +155,22 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
     CompactionStrategy compactionStrategy = defaultCompactionStrategy,
     bool crashRecovery = true,
     String? path,
+    String? collection,
     @Deprecated('Use encryptionCipher instead') List<int>? encryptionKey,
   }) async {
     if (encryptionKey != null) {
       encryptionCipher = HiveAesCipher(encryptionKey);
     }
-    return await _openBox<E>(name, true, encryptionCipher, keyComparator,
-        compactionStrategy, crashRecovery, path, null) as LazyBox<E>;
+    return await _openBox<E>(
+        name,
+        true,
+        encryptionCipher,
+        keyComparator,
+        compactionStrategy,
+        crashRecovery,
+        path,
+        null,
+        collection) as LazyBox<E>;
   }
 
   BoxBase<E> _getBoxInternal<E>(String name, [bool? lazy]) {
@@ -211,13 +226,14 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   }
 
   @override
-  Future<void> deleteBoxFromDisk(String name, {String? path}) async {
+  Future<void> deleteBoxFromDisk(String name,
+      {String? path, String? collection}) async {
     var lowerCaseName = name.toLowerCase();
     var box = _boxes[lowerCaseName];
     if (box != null) {
       await box.deleteFromDisk();
     } else {
-      await _manager.deleteBox(lowerCaseName, path ?? homePath);
+      await _manager!.deleteBox(lowerCaseName, path ?? homePath, collection);
     }
   }
 
@@ -236,8 +252,10 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   }
 
   @override
-  Future<bool> boxExists(String name, {String? path}) async {
+  Future<bool> boxExists(String name,
+      {String? path, String? collection}) async {
     var lowerCaseName = name.toLowerCase();
-    return await _manager.boxExists(lowerCaseName, path ?? homePath);
+    return await _manager!
+        .boxExists(lowerCaseName, path ?? homePath, collection);
   }
 }

hive/test/integration/recovery_test.dart
@@ -46,7 +46,6 @@ Future _performTest(bool lazy) async {
       await box.close();
     }
 
-    print((await boxFile.readAsBytes()).length);
     expect(await boxFile.readAsBytes(), getFrameBytes(subFrames));
   }
 }

.gitignore
@@ -94,6 +94,9 @@ unlinked_spec.ds
 # Coverage
 coverage/
 
+# JavaScipt
+web_worker.dart.js*
+
 # Exceptions to above rules.
 !**/ios/**/default.mode1v3
 !**/ios/**/default.mode2v3

hive/.pubignore
@@ -1,0 +1,1 @@
+!lib/src/backend/js/web_worker/web_worker.dart.js*

hive/lib/hive.dart
@@ -18,23 +18,25 @@ import 'package:hive/src/object/hive_object.dart';
 import 'package:hive/src/util/extensions.dart';
 import 'package:meta/meta.dart';
 
+export 'src/box_collection.dart';
 export 'src/object/hive_object.dart' show HiveObject, HiveObjectMixin;
 
 part 'src/annotations/hive_field.dart';
 part 'src/annotations/hive_type.dart';
 part 'src/binary/binary_reader.dart';
 part 'src/binary/binary_writer.dart';
-part 'src/box/box_base.dart';
 part 'src/box/box.dart';
+part 'src/box/box_base.dart';
 part 'src/box/lazy_box.dart';
 part 'src/crypto/hive_aes_cipher.dart';
 part 'src/crypto/hive_cipher.dart';
+part 'src/hive.dart';
+part 'src/hive_error.dart';
 part 'src/object/hive_collection.dart';
 part 'src/object/hive_list.dart';
-part 'src/registry/type_registry.dart';
+part 'src/object/hive_storage_backend_preference.dart';
 part 'src/registry/type_adapter.dart';
-part 'src/hive_error.dart';
-part 'src/hive.dart';
+part 'src/registry/type_registry.dart';
 
 /// Global constant to access Hive.
 // ignore: non_constant_identifier_names

hive/lib/src/backend/js/backend_manager.dart
@@ -1,46 +1,18 @@
-import 'dart:html';
-import 'dart:indexed_db';
-import 'dart:js' as js;
 import 'package:hive/hive.dart';
-import 'package:hive/src/backend/js/storage_backend_js.dart';
 import 'package:hive/src/backend/storage_backend.dart';
 
+import 'native/backend_manager.dart' as native;
+
 /// Opens IndexedDB databases
-class BackendManager implements BackendManagerInterface {
-  IdbFactory? get indexedDB => js.context.hasProperty('window')
-      ? window.indexedDB
-      : WorkerGlobalScope.instance.indexedDB;
+abstract class BackendManager {
+  BackendManager._();
 
-  @override
-  Future<StorageBackend> open(
-      String name, String? path, bool crashRecovery, HiveCipher? cipher) async {
-    var db = await indexedDB!.open(name, version: 1, onUpgradeNeeded: (e) {
-      var db = e.target.result as Database;
-      if (!db.objectStoreNames!.contains('box')) {
-        db.createObjectStore('box');
-      }
-    });
-
-    return StorageBackendJs(db, cipher);
-  }
-
-  @override
-  Future<void> deleteBox(String name, String? path) {
-    return indexedDB!.deleteDatabase(name);
-  }
-
-  @override
-  Future<bool> boxExists(String name, String? path) async {
-    // https://stackoverflow.com/a/17473952
-    try {
-      var _exists = true;
-      await indexedDB!.open(name, version: 1, onUpgradeNeeded: (e) {
-        e.target.transaction!.abort();
-        _exists = false;
-      });
-      return _exists;
-    } catch (error) {
-      return false;
+  // dummy implementation as the WebWorker branch is not stable yet
+  static BackendManagerInterface select(
+      [HiveStorageBackendPreference? backendPreference]) {
+    switch (backendPreference) {
+      default:
+        return native.BackendManager();
     }
   }
 }

hive/lib/src/backend/js/native/backend_manager.dart
@@ -1,0 +1,82 @@
+import 'dart:async';
+import 'dart:html';
+import 'dart:indexed_db';
+import 'dart:js' as js;
+import 'package:hive/hive.dart';
+import 'package:hive/src/backend/js/native/storage_backend_js.dart';
+import 'package:hive/src/backend/storage_backend.dart';
+
+/// Opens IndexedDB databases
+class BackendManager implements BackendManagerInterface {
+  IdbFactory? get indexedDB => js.context.hasProperty('window')
+      ? window.indexedDB
+      : WorkerGlobalScope.instance.indexedDB;
+
+  @override
+  Future<StorageBackend> open(String name, String? path, bool crashRecovery,
+      HiveCipher? cipher, String? collection) async {
+    // compatibility for old store format
+    final databaseName = collection ?? name;
+    final objectStoreName = collection == null ? 'box' : name;
+
+    final db =
+        await indexedDB!.open(databaseName, version: 1, onUpgradeNeeded: (e) {
+      var db = e.target.result as Database;
+      if (!(db.objectStoreNames ?? []).contains(objectStoreName)) {
+        db.createObjectStore(objectStoreName);
+      }
+    });
+
+    return StorageBackendJs(db, cipher, objectStoreName);
+  }
+
+  @override
+  Future<void> deleteBox(String name, String? path, String? collection) async {
+    // compatibility for old store format
+    final databaseName = collection ?? name;
+    final objectStoreName = collection == null ? 'box' : name;
+
+    // directly deleting the entire DB if a non-collection Box
+    if (collection == null) {
+      await indexedDB!.deleteDatabase(databaseName);
+    } else {
+      final db =
+          await indexedDB!.open(databaseName, version: 1, onUpgradeNeeded: (e) {
+        var db = e.target.result as Database;
+        if ((db.objectStoreNames ?? []).contains(objectStoreName)) {
+          db.deleteObjectStore(objectStoreName);
+        }
+      });
+      if ((db.objectStoreNames ?? []).isEmpty) {
+        indexedDB!.deleteDatabase(databaseName);
+      }
+    }
+  }
+
+  @override
+  Future<bool> boxExists(String name, String? path, String? collection) async {
+    // compatibility for old store format
+    final databaseName = collection ?? name;
+    final objectStoreName = collection == null ? 'box' : name;
+    // https://stackoverflow.com/a/17473952
+    try {
+      var _exists = true;
+      if (collection == null) {
+        await indexedDB!.open(databaseName, version: 1, onUpgradeNeeded: (e) {
+          e.target.transaction!.abort();
+          _exists = false;
+        });
+      } else {
+        final db =
+            await indexedDB!.open(collection, version: 1, onUpgradeNeeded: (e) {
+          var db = e.target.result as Database;
+          _exists = (db.objectStoreNames ?? []).contains(objectStoreName);
+        });
+        _exists = (db.objectStoreNames ?? []).contains(objectStoreName);
+      }
+      return _exists;
+    } catch (error) {
+      return false;
+    }
+  }
+}

hive/lib/src/backend/js/native/box_collection_indexed_db.dart
@@ -1,0 +1,239 @@
+import 'dart:html';
+import 'dart:indexed_db';
+
+import 'package:hive/hive.dart';
+
+/// represents a [BoxCollection] for raw use with indexed DB
+class BoxCollection {
+  final Database _db;
+  final Set<String> boxNames;
+
+  BoxCollection(this._db, this.boxNames);
+
+  static Future<BoxCollection> open(
+    String name,
+    Set<String> boxNames, {
+    dynamic path,
+    HiveCipher? key,
+  }) async {
+    final factory = window.indexedDB;
+    if (factory == null) {
+      throw Exception(
+          'Unable to open FluffyBox collection - IndexedDB not supported in this browser!');
+    }
+    final _db = await factory.open(name, version: 1,
+        onUpgradeNeeded: (VersionChangeEvent event) {
+      final _db = event.target.result;
+      for (final name in boxNames) {
+        _db.createObjectStore(name, autoIncrement: true);
+      }
+    });
+    return BoxCollection(_db, boxNames);
+  }
+
+  Future<CollectionBoxIndexedDb<V>> openBox<V>(String name,
+      {bool preload = false,
+      CollectionBoxIndexedDb<V> Function(String, BoxCollection)?
+          boxCreator}) async {
+    if (!boxNames.contains(name)) {
+      throw Exception(
+          'Box with name $name is not in the known box names of this collection.');
+    }
+    final i = _openBoxes.indexWhere((box) => box.name == name);
+    if (i != -1) {
+      return _openBoxes[i] as CollectionBoxIndexedDb<V>;
+    }
+    final box =
+        boxCreator?.call(name, this) ?? CollectionBoxIndexedDb<V>(name, this);
+    if (preload) {
+      box._cache.addAll(await box.getAllValues());
+    }
+    _openBoxes.add(box);
+    return box;
+  }
+
+  final List<CollectionBoxIndexedDb> _openBoxes = [];
+
+  List<Future<void> Function(Transaction txn)>? _txnCache;
+
+  Future<void> transaction(
+    Future<void> Function() action, {
+    List<String>? boxNames,
+    bool readOnly = false,
+  }) async {
+    boxNames ??= this.boxNames.toList();
+    if (_txnCache != null) {
+      await action();
+      return;
+    }
+    _txnCache = [];
+    await action();
+    final cache =
+        List<Future<void> Function(Transaction txn)>.from(_txnCache ?? []);
+    _txnCache = null;
+    if (cache.isEmpty) return;
+    final txn = _db.transaction(boxNames, readOnly ? 'readonly' : 'readwrite');
+    for (final fun in cache) {
+      fun(txn);
+    }
+    await txn.completed;
+    return;
+  }
+
+  void close() => _db.close();
+
+  Future<void> deleteFromDisk() async {
+    final factory = window.indexedDB;
+    for (final box in _openBoxes) {
+      box._cache.clear();
+      box._cachedKeys = null;
+    }
+    _openBoxes.clear();
+    _db.close();
+    if (factory == null || _db.name == null) {
+      throw Exception('Unable to delete fluffybox collection');
+    }
+    factory.deleteDatabase(_db.name!);
+  }
+}
+
+class CollectionBoxIndexedDb<V> {
+  final String name;
+  final BoxCollection boxCollection;
+  final Map<String, V?> _cache = {};
+  Set<String>? _cachedKeys;
+
+  CollectionBoxIndexedDb(this.name, this.boxCollection) {
+    if (!(V is String ||
+        V is int ||
+        V is Object ||
+        V is List<Object?> ||
+        V is Map<String, Object?> ||
+        V is double)) {
+      throw Exception(
+          'Value type ${V.runtimeType} is not one of the allowed value types {String, int, double, List<Object?>, Map<String, Object?>}.');
+    }
+  }
+
+  Future<List<String>> getAllKeys([Transaction? txn]) async {
+    final cachedKey = _cachedKeys;
+    if (cachedKey != null) return cachedKey.toList();
+    txn ??= boxCollection._db.transaction(name, 'readonly');
+    final store = txn.objectStore(name);
+    final request = store.getAllKeys(null);
+    await request.onSuccess.first;
+    final List<String> keys =
+        List.from(request.result.cast<String>() as Iterable);
+    _cachedKeys = keys.toSet();
+    return keys;
+  }
+
+  Future<Map<String, V>> getAllValues([Transaction? txn]) async {
+    txn ??= boxCollection._db.transaction(name, 'readonly');
+    final store = txn.objectStore(name);
+    final map = <String, V>{};
+    final cursorStream = store.openCursor(autoAdvance: true);
+    await for (final cursor in cursorStream) {
+      map[cursor.key as String] = cursor.value as V;
+    }
+    return map;
+  }
+
+  Future<V?> get(String key, [Transaction? txn]) async {
+    if (_cache.containsKey(key)) return _cache[key];
+    txn ??= boxCollection._db.transaction(name, 'readonly');
+    final store = txn.objectStore(name);
+    _cache[key] = await store.getObject(key) as V?;
+    return _cache[key];
+  }
+
+  Future<List<V?>> getAll(List<String> keys, [Transaction? txn]) async {
+    if (!keys.any((key) => !_cache.containsKey(key))) {
+      return keys.map((key) => _cache[key]).toList();
+    }
+    txn ??= boxCollection._db.transaction(name, 'readonly');
+    final store = txn.objectStore(name);
+    final list = await Future.wait(keys.map(store.getObject));
+    for (var i = 0; i < keys.length; i++) {
+      _cache[keys[i]] = list[i] as V?;
+    }
+    return list.cast<V?>();
+  }
+
+  Future<void> put(String key, V val, [Transaction? txn]) async {
+    if (val == null) {
+      return delete(key, txn);
+    }
+    final txnCache = boxCollection._txnCache;
+    if (txnCache != null) {
+      txnCache.add((txn) => put(key, val, txn));
+      _cache[key] = val;
+      _cachedKeys?.add(key);
+      return;
+    }
+
+    txn ??= boxCollection._db.transaction(name, 'readwrite');
+    final store = txn.objectStore(name);
+    await store.put(val, key);
+    _cache[key] = val;
+    _cachedKeys?.add(key);
+    return;
+  }
+
+  Future<void> delete(String key, [Transaction? txn]) async {
+    final txnCache = boxCollection._txnCache;
+    if (txnCache != null) {
+      txnCache.add((txn) => delete(key, txn));
+      _cache[key] = null;
+      _cachedKeys?.remove(key);
+      return;
+    }
+
+    txn ??= boxCollection._db.transaction(name, 'readwrite');
+    final store = txn.objectStore(name);
+    await store.delete(key);
+    _cache[key] = null;
+    _cachedKeys?.remove(key);
+    return;
+  }
+
+  Future<void> deleteAll(List<String> keys, [Transaction? txn]) async {
+    final txnCache = boxCollection._txnCache;
+    if (txnCache != null) {
+      txnCache.add((txn) => deleteAll(keys, txn));
+      for (var key in keys) {
+        _cache[key] = null;
+      }
+      _cachedKeys?.removeAll(keys);
+      return;
+    }
+
+    txn ??= boxCollection._db.transaction(name, 'readwrite');
+    final store = txn.objectStore(name);
+    for (final key in keys) {
+      await store.delete(key);
+      _cache[key] = null;
+      _cachedKeys?.removeAll(keys);
+    }
+    return;
+  }
+
+  Future<void> clear([Transaction? txn]) async {
+    final txnCache = boxCollection._txnCache;
+    if (txnCache != null) {
+      txnCache.add(clear);
+      _cache.clear();
+      _cachedKeys = null;
+      return;
+    }
+
+    txn ??= boxCollection._db.transaction(name, 'readwrite');
+    final store = txn.objectStore(name);
+    await store.clear();
+    _cache.clear();
+    _cachedKeys = null;
+    return;
+  }
+
+  Future<void> flush() => Future.value();
+}

commit 537ddaf3d8228ddcfaf554d85ad523d35578f1cb

Fix Regression #977 - Web not registering DateTimeAdapter
Regression was caused in #956 (4dbcd76) where `HiveImpl` no longer
called `_registerDefaultAdapters`, assuming that it would happen in
`init`, which is not necessary for web.

Also remove `HiveImpl.debug` and `HiveImpl.test`.

`HiveImpl.test` was masking some odd legacy behavior in tests, so this
fixes the test.
`HiveImpl.debug` is not used anywhere, and its funcationality can be
accomplished using `init`

hive/lib/src/hive_impl.dart
@@ -33,19 +33,8 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   String? homePath;
 
   /// Not part of public API
-  HiveImpl();
-
-  /// Not part of public API
-  @visibleForTesting
-  HiveImpl.debug(this._managerOverride) {
+  HiveImpl() {
     _registerDefaultAdapters();
-  }
-
-  /// Not part of public API
-  @visibleForTesting
-  HiveImpl.test() : _managerOverride = BackendManager.select() {
-    registerAdapter(DateTimeAdapter<DateTime>(), internal: true);
-    registerAdapter(BigIntAdapter(), internal: true);
   }
 
   /// either returns the preferred [BackendManagerInterface] or the
@@ -67,7 +56,6 @@ class HiveImpl extends TypeRegistryImpl implements HiveInterface {
   }) {
     homePath = path;
     _managerOverride = BackendManager.select(backendPreference);
-    _registerDefaultAdapters();
   }
 
   Future<BoxBase<E>> _openBox<E>(

hive/test/integration/recovery_test.dart
@@ -20,7 +20,7 @@ Future _performTest(bool lazy) async {
   framesSetLengthOffset(frames, frameBytes);
 
   var dir = await getTempDir();
-  var hive = HiveImpl.test();
+  var hive = HiveImpl();
   hive.init(dir.path);
 
   for (var i = 0; i < bytes.length; i++) {

hive/test/tests/frames.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:hive/hive.dart';
+import 'package:hive/src/adapters/date_time_adapter.dart';
 import 'package:hive/src/binary/binary_writer_impl.dart';
 import 'package:hive/src/binary/frame.dart';
 import 'package:hive/src/hive_impl.dart';
@@ -12,7 +13,7 @@ export '../generated/frame_values_encrypted.g.dart';
 export '../generated/frames.g.dart';
 export '../generated/frames_encrypted.g.dart';
 
-TypeRegistry get testRegistry => HiveImpl.test();
+TypeRegistry get testRegistry => HiveImpl();
 
 class _HiveAesCipherStaticIV extends HiveAesCipher {
   _HiveAesCipherStaticIV() : super(Uint8List.fromList(List.filled(32, 1)));
@@ -78,8 +79,8 @@ List<Frame> get testFrames => <Frame>[
         'Map': {'Key': 'Val', 'Key2': 2}
       }),
       Frame('DateTime test', [
-        DateTime.fromMillisecondsSinceEpoch(0),
-        DateTime.fromMillisecondsSinceEpoch(1566656623020),
+        DateTimeWithoutTZ.fromMillisecondsSinceEpoch(0),
+        DateTimeWithoutTZ.fromMillisecondsSinceEpoch(1566656623020),
       ]),
       Frame('BigInt Test',
           BigInt.parse('1234567890123456789012345678901234567890'))